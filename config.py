f564793fc057a5a8468078870da87fffde9f03fa
Joshua Chou <joshuachou912@gmail.com>
2023-11-04 10:09:42 -0400

    added env varibles

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
-from flask_session import Session
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 class Config:
-    SECRET_KEY = os.urandom(16).hex()
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SESSION_TYPE = 'filesystem'  # Specifies the type of session interface to use
+    SECRET_KEY = os.getenv('SECRET_KEY') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
+    SESSION_TYPE = 'filesystem'  
     SESSION_PERMANENT = False  # If True, makes the session last indefinitely. Default is 31 days
-    # SESSION_USE_SIGNER = True  # Helps protect the user's session cookie
-    # SESSION_KEY_PREFIX = 'my_app:'  # Prefix for the session cookie name, to avoid conflicts
-    # SESSION_FILE_DIR = 'session_data'
-
+ 
